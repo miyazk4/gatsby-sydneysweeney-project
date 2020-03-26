1bd736c00a756c09f74f959eddcc7c4f09a8e816
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-26 16:29:59 +0000

    fetched md files for specific paths

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,10 +1,29 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Works = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/works/" } }) {
+        nodes {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+
   return (
     <Layout>
-      <h1>I'm going to have Sydney's works in here.</h1>
+      <h1>Works</h1>
+      <div>
+        {data.allMarkdownRemark.nodes.map(nodes => (
+          <div>{nodes.frontmatter.title}</div>
+        ))}
+      </div>
     </Layout>
   )
 }
---
+++ src/pages/works/euphoria.md
@@ -0,0 +1,8 @@
+---
+title: "Euphoria"
+year: "2019"
+briefDescription: "Filmed in 2019"
+featuredImage: "../images/sydbooks.jpg"
+---
+
+Euphoria tv show
