9816f4b2892a2027e38d2d93597bfdb3ad2adbf7
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-26 14:44:40 +0000

    Added some style to individual new

--- src/templates/news.tsx
+++ src/templates/news.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import newStyles from "./new.module.scss"
 
 export const query = graphql`
   query($slug: String!) {
@@ -25,17 +26,29 @@ export const query = graphql`
 const News = props => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <Img
-        fluid={
-          props.data.markdownRemark.frontmatter.featuredImage.childImageSharp
-            .fluid
-        }
-      />
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <div className={newStyles.wrapper}>
+        <div className={newStyles.newContainer}>
+          <h1 className={newStyles.title}>
+            {props.data.markdownRemark.frontmatter.title}
+          </h1>
+          <p className={newStyles.date}>
+            {props.data.markdownRemark.frontmatter.date}
+          </p>
+          <div className={newStyles.imageContainer}>
+            <Img
+              className={newStyles.image}
+              fluid={
+                props.data.markdownRemark.frontmatter.featuredImage
+                  .childImageSharp.fluid
+              }
+            />
+          </div>
+          <div
+            className={newStyles.content}
+            dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+          ></div>
+        </div>
+      </div>
     </Layout>
   )
 }
