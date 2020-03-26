7a50b737ecb7f252f3faf46aa14c8ea636bdb99c
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-26 23:47:05 +0000

    Added some md files to works and called them

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import worksStyles from "./works.module.scss"
 
 const Works = () => {
   const data = useStaticQuery(graphql`
@@ -9,6 +11,17 @@ const Works = () => {
         nodes {
           frontmatter {
             title
+            year
+            briefDescription
+            type
+            seasons
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
@@ -18,11 +31,36 @@ const Works = () => {
 
   return (
     <Layout>
-      <h1>Works</h1>
-      <div>
-        {data.allMarkdownRemark.nodes.map(nodes => (
-          <div>{nodes.frontmatter.title}</div>
-        ))}
+      <div className={worksStyles.wrapper}>
+        <h1 className={worksStyles.title}>Works</h1>
+        <div className={worksStyles.workContainer}>
+          {data.allMarkdownRemark.nodes.map(nodes => (
+            <div className={worksStyles.worksBox}>
+              <h2 className={worksStyles.subtitle}>
+                {nodes.frontmatter.title}
+              </h2>
+              <div className={worksStyles.showDetails}>
+                <div className={worksStyles.yearType}>
+                  <span>{nodes.frontmatter.type} </span>
+                  <span>({nodes.frontmatter.year})</span>
+                </div>
+                <p className={worksStyles.seasons}>
+                  Seasons: {nodes.frontmatter.seasons}
+                </p>
+              </div>
+              <div className={worksStyles.imageContainer}>
+                <Img
+                  className={worksStyles.image}
+                  fluid={nodes.frontmatter.featuredImage.childImageSharp.fluid}
+                  alt=""
+                />
+              </div>
+              <p className={worksStyles.information}>
+                {nodes.frontmatter.briefDescription}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   )
--- src/pages/works/handmaidstale.md
+++ src/pages/works/handmaidstale.md
@@ -1,10 +1,10 @@
 ---
-title: "Euphoria"
-year: "2019-"
-seasons: "1"
-featuredImage: "../../images/works/euphoria1.jpeg"
+title: "Handmaid's Tale"
+year: "2016-"
+seasons: "3"
+featuredImage: "../../images/works/handmaidstale1.jpg"
 type: "Tv Show"
-briefDescription: "Euphoria follows a group of high school students through their experiences of sex, drugs, friendships, love, identity and trauma."
+briefDescription: "The Handmaid's Tale is an American dystopian tragedy web television series created by Bruce Miller, based on the 1985 novel of the same name by Canadian author, Margaret Atwood."
 ---
 
 Euphoria is an American teen drama television series created by Sam Levinson, loosely based on the Israeli miniseries of the same name.[1][2] Euphoria follows a group of high school students through their experiences of sex, drugs, friendships, love, identity and trauma.[3] The series stars Zendaya, Hunter Schafer and Jacob Elordi, and premiered on HBO on June 16, 2019.[4][5] In July 2019, the series was renewed for a second season.[6]
