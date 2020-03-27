c8b436759f4f2d0f351b929305bc11561d135853
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-27 02:50:07 +0000

    Basic content and styling to contacts

--- src/pages/about/contacts.tsx
+++ src/pages/about/contacts.tsx
@@ -1,15 +1,28 @@
 import React from "react"
 import Layout from "../../components/layout"
+import contactStyles from "./contact.module.scss"
 
 const Contacts = () => {
   return (
     <Layout>
-      <div>
-        <h1>Management</h1>
-        <div>
-          <p>Phone:</p>
-          <p>Location:</p>
-          <p>Email:</p>
+      <div className={contactStyles.wrapper}>
+        <div className={contactStyles.contactContainer}>
+          <h1 className={contactStyles.title}>Booking</h1>
+          <div className={contactStyles.informationContainer}>
+            <p className={contactStyles.information}>
+              For booking with Sydney Sweeney please reach out to one or more of
+              the followings:
+            </p>
+            <div>
+              <p className={contactStyles.information}>Phone: 000-123-4567</p>
+              <p className={contactStyles.information}>
+                Location: Los Angeles, California
+              </p>
+              <p className={contactStyles.information}>
+                Email: sydneymanagement @ something.com
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
